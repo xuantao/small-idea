b5f46410734178e8bd296812774f65e8810289c2
xuantao <xuantao1@kingsoft.com>
2017-07-03 11:13:56 +0800

    convert json file 2 cs&cpp define and load

---
+++ json_2_cs_cpp/exporter/Exporter/CppExporter.cs
@@ -0,0 +1,382 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+public class CppExporter
+{
+    class ArrayType
+    {
+        public IDeclear Declear;
+        public string Type;
+
+        public ArrayType(IDeclear declear, string type)
+        {
+            Declear = declear;
+            Type = type;
+        }
+    }
+
+    TextWriter _header;
+    TextWriter _cpp;
+    string _name;
+    IDeclear _root;
+    IDeclear[] _manuals;
+
+    TextWriter _writer;
+
+    Dictionary<string, string> _dicVector = new Dictionary<string, string>();
+    
+    List<ArrayType> _cppArrays = new List<ArrayType>();
+
+    CppExporter(TextWriter header, TextWriter cpp, string name, JsonReport report)
+    {
+        _header = header;
+        _cpp = cpp;
+        _name = name;
+        _root = report.Root;
+        _manuals = report.Manuals;
+
+        if (_manuals == null)
+            _manuals = new IDeclear[0];
+    }
+
+    void ExportHeader()
+    {
+        _writer.WriteLine("/*");
+        _writer.WriteLine(" * this file is auto generated, please do not edit it manual!");
+        _writer.WriteLine(" * {0}", System.DateTime.Now);
+        _writer.WriteLine("*/");
+        _writer.WriteLine();
+
+        _writer.WriteLine("#pragma once");
+        _writer.WriteLine("#include <vector>");
+        _writer.WriteLine("#include <string>");
+        _writer.WriteLine();
+
+        _writer.WriteLine("class {0}", _name);
+        _writer.WriteLine("{");
+
+        if (_manuals.Length > 0)
+        {
+            _writer.WriteLine("public:");
+
+            foreach (var declear in _manuals)
+            {
+                WriteTab(1);
+                _writer.WriteLine("struct {0}", declear.Type);
+                WriteTab(1);
+                _writer.WriteLine("{");
+
+                DefineMember(declear, 2);
+
+                WriteTab(1);
+                _writer.WriteLine("};");
+                _writer.WriteLine();
+            }
+            //_writer.WriteLine();
+        }
+
+        // define constructer
+        _writer.WriteLine("public:");
+        WriteTab(1);
+        _writer.WriteLine("{0}();", _name);
+        //_writer.WriteLine("~{0}() {}", _name);
+
+        // define loader member function
+        _writer.WriteLine();
+        _writer.WriteLine("public:");
+        WriteTab(1);
+        _writer.WriteLine("bool LoadJson(const char* lpData, int nSize);");
+
+        // define member data
+        if (_root.Members.Length > 0)
+        {
+            _writer.WriteLine();
+            _writer.WriteLine("public:");
+
+            DefineMember(_root, 1);
+        }
+
+        _writer.WriteLine("};");
+        _writer.WriteLine();
+    }
+
+    void DefineMember(IDeclear declear, int tab)
+    {
+        foreach (var mem in declear.Members)
+        {
+            WriteTab(tab);
+            _writer.WriteLine("{0} {1};", GetTypeName(mem), mem.Key);
+        }
+    }
+
+    void ExportCpp()
+    {
+        _writer.WriteLine("#include \"{0}.h\"", _name);
+        _writer.WriteLine("#include <libjson.h>");
+        _writer.WriteLine();
+
+        foreach (var declear in _manuals)
+        {
+            _writer.WriteLine("static bool sLoad(const JSONNode& node, {0}::{1}& ret);", _name, declear.Type);
+        }
+
+        foreach (var pair in _cppArrays)
+        {
+            _writer.WriteLine("static bool sLoad(const JSONNode& node, {1}& ret);", _name, pair.Type);
+        }
+
+        foreach (var declear in _manuals)
+        {
+            _writer.WriteLine();
+            WriteObjLoader(declear, 0);
+        }
+
+        foreach (var pair in _cppArrays)
+        {
+            _writer.WriteLine();
+            WriteArrayLoader(pair.Declear, pair.Type, 0);
+        }
+
+        _writer.WriteLine();
+
+    }
+
+    void WriteLoader()
+    {
+        _writer.WriteLine("bool {0}::LoadJson(const char* lpJson, int nSize)", _name);
+        _writer.WriteLine("{");
+
+        WriteTab(1);
+        _writer.WriteLine("if (lpJson == null)");
+        WriteTab(2);
+        _writer.WriteLine("return false;");
+
+
+
+        WriteTab(1);
+        _writer.WriteLine("return true;");
+        _writer.WriteLine("}");
+    }
+
+    void WriteObjLoader(IDeclear declear, int tab)
+    {
+        WriteTab(tab);
+        _writer.WriteLine("static bool sLoad(const JSONNode& node, {0}::{1}& ret)", _name, declear.Type);
+
+        WriteTab(tab);
+        _writer.WriteLine("{");
+        if (declear.Members.Length == 0)
+        {
+            WriteTab(tab + 1);
+            _writer.WriteLine("return true;");
+
+            WriteTab(tab);
+            _writer.WriteLine("}");
+            return;
+        }
+
+        WriteTab(tab + 1);
+        _writer.WriteLine("if (node.type() != JSON_NODE)");
+        WriteTab(tab + 2);
+        _writer.WriteLine("return false;");
+        _writer.WriteLine();
+
+        WriteTab(tab + 1);
+        _writer.WriteLine("for (JSONNode::const_iterator it = node.begin(); it != node.end(); ++it)");
+        WriteTab(tab + 1);
+        _writer.WriteLine("{");
+
+        WriteTab(tab + 2);
+        _writer.WriteLine("std::string node_name = it->name();");
+
+        bool bFirst = true;
+        foreach (var mem in declear.Members)
+        {
+            WriteTab(tab + 2);
+            if (!bFirst)
+                _writer.Write("else ");
+            _writer.WriteLine("if (node_name == \"{0}\")", mem.Key);
+            bFirst = false;
+
+            WriteTab(tab + 2);
+            _writer.WriteLine("{");
+            if (mem.DeclearType == DeclearType.Basic)
+                LoadBasic(mem, tab + 3, "(*it)", string.Format("ret.{0}", mem.Key));
+            else
+                LoadObj(mem, tab + 3, "(*it)", string.Format("ret.{0}", mem.Key));
+            WriteTab(tab + 2);
+            _writer.WriteLine("}");
+        }
+
+        WriteTab(tab + 1);
+        _writer.WriteLine("}");
+
+        _writer.WriteLine();
+        WriteTab(tab + 1);
+        _writer.WriteLine("return true;");
+
+        WriteTab(tab);
+        _writer.WriteLine("}");
+    }
+
+    void WriteArrayLoader(IDeclear declear, string type, int tab)
+    {
+        WriteTab(tab);
+        _writer.WriteLine("static bool sLoad(const JSONNode& node, {0}& ret)", type);
+
+        WriteTab(tab);
+        _writer.WriteLine("{");
+
+        WriteTab(tab + 1);
+        _writer.WriteLine("if (node.type() != JSON_ARRAY)");
+        WriteTab(tab + 2);
+        _writer.WriteLine("return false;");
+        _writer.WriteLine();
+
+        WriteTab(tab + 1);
+        _writer.WriteLine("for (int i = 0; i < node.size(); ++i)");
+        WriteTab(tab + 1);
+        _writer.WriteLine("{");
+
+        IDeclear real = declear.Members[0];
+        if (real.DeclearType == DeclearType.Basic)
+            LoadBasic(real, tab + 2, "node[i]", "ret[i]");
+        else
+            LoadObj(real, tab + 2, "node[i]", "ret[i]");
+
+        WriteTab(tab + 1);
+        _writer.WriteLine("}");
+
+        _writer.WriteLine();
+        WriteTab(tab + 1);
+        _writer.WriteLine("return true;");
+
+        WriteTab(tab);
+        _writer.WriteLine("}");
+    }
+
+    void LoadBasic(IDeclear declear, int tab, string node, string ret)
+    {
+        if (declear.Type == "int" || declear.Type == "long")
+        {
+            WriteTab(tab);
+            _writer.WriteLine("if ({0}.type() == JSON_NUMBER)", node);
+            WriteTab(tab + 1);
+            _writer.WriteLine("{0} = {1}.as_int();", ret, node);
+        }
+        else if (declear.Type == "bool")
+        {
+            WriteTab(tab);
+            _writer.WriteLine("if ({0}.type() == JSON_BOOL)", node);
+            WriteTab(tab + 1);
+            _writer.WriteLine("{0} = {1}.as_bool();", ret, node);
+        }
+        else if (declear.Type == "float")
+        {
+            WriteTab(tab);
+            _writer.WriteLine("if ({0}.type() == JSON_NUMBER)", node);
+            WriteTab(tab + 1);
+            _writer.WriteLine("{0} = {1}.as_float();", ret, node);
+        }
+        else if (declear.Type == "string")
+        {
+            WriteTab(tab);
+            _writer.WriteLine("if ({0}.type() == JSON_STRING)", node);
+            WriteTab(tab + 1);
+            _writer.WriteLine("{0} = {1}.as_string();", ret, node);
+        }
+    }
+
+    void LoadObj(IDeclear declear, int tab, string node, string ret)
+    {
+        WriteTab(tab);
+        _writer.WriteLine("sLoad({0}, {1});", node, ret);
+    }
+
+    void AnalysisCppTypes()
+    {
+        foreach (var type in _manuals)
+        {
+            foreach (var mem in type.Members)
+            {
+                if (mem.DeclearType != DeclearType.Array)
+                    continue;
+
+                GenArrayTypes(mem);
+            }
+        }
+
+        foreach (var mem in _root.Members)
+        {
+            if (mem.DeclearType != DeclearType.Array)
+                continue;
+
+            GenArrayTypes(mem);
+        }
+
+        _dicVector.Add("string", "std::string");
+    }
+
+    string GetTypeName(IDeclear declear)
+    {
+        string name = string.Empty;
+        if (_dicVector.TryGetValue(declear.Type, out name))
+            return name;
+        return declear.Type;
+    }
+
+    bool IsBasic(IDeclear declear)
+    {
+        foreach (var mem in declear.Members)
+        {
+            if (mem.DeclearType == DeclearType.Object)
+                return false;
+        }
+        return true;
+    }
+
+    void WriteTab(int tab)
+    {
+        _writer.Write(new string(' ', tab * 4));
+    }
+
+    void GenArrayTypes(IDeclear declear)
+    {
+        if (_dicVector.ContainsKey(declear.Type))
+            return;
+
+        string real = declear.Type.Substring(0, declear.Type.Length - 2);
+        if (real.EndsWith("[]"))
+        {
+            GenArrayTypes(declear.Members[0]);
+            _dicVector.Add(declear.Type, string.Format("std::vector<{0} >", _dicVector[real]));
+            _cppArrays.Add(new ArrayType(declear, string.Format("std::vector<{0} >", _dicVector[real])));
+        }
+        else
+        {
+            _dicVector.Add(declear.Type, string.Format("std::vector<{0}>", real));
+            _cppArrays.Add(new ArrayType(declear, string.Format("std::vector<{0}::{1}>", _name, real)));
+        }
+    }
+
+    bool Export()
+    {
+        AnalysisCppTypes();
+
+        _writer = _header;
+        //ExportHeader();
+
+        _writer = _cpp;
+        ExportCpp();
+        return true;
+    }
+
+    public static bool Export(TextWriter header, TextWriter cpp, string name, JsonReport report)
+    {
+        if (report == null || report.Root == null)
+            return false;
+
+        CppExporter exportor = new CppExporter(header, cpp, name, report);
+        return exportor.Export();
+    }
+}
---
+++ json_2_cs_cpp/exporter/Exporter/ExportJson.cs
@@ -0,0 +1,427 @@
+﻿using LitJson;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+public interface IJsonWalker
+{
+    void OnObjectBegin();
+    void OnObjectEnd();
+
+    void OnArrayBegin();
+    void OnArrayEnd();
+
+    void OnKey(string key);
+    void OnIndex(int index);
+    void OnValue(bool value);
+    void OnValue(int value);
+    void OnValue(long value);
+    void OnValue(float value);
+    void OnValue(string value);
+}
+
+public enum DeclearType
+{
+    Basic,
+    Object,
+    Array,
+}
+
+public interface IDeclear
+{
+    string Type { get; }
+    string Key { get; }
+    object Def { get; }
+    DeclearType DeclearType { get; }
+    int MemberCount { get; }
+    IDeclear[] Members { get; }
+}
+
+internal class ObjectDeclear : IDeclear, IEquatable<ObjectDeclear>
+{
+    string _key;
+    List<IDeclear> _members = new List<IDeclear>();
+
+    public ObjectDeclear(string key) { _key = key; }
+
+    public string Type { get; set; }
+    public string Key { get { return _key; } }
+    public object Def { get { return GetDef(); } }
+    public DeclearType DeclearType { get { return DeclearType.Object; } }
+
+    public int MemberCount { get { return _members.Count; } }
+    public IDeclear[] Members { get { return _members.ToArray(); } }
+    public bool IsBasic()
+    {
+        foreach (IDeclear mem in _members)
+        {
+            if (mem.DeclearType == DeclearType.Object)
+                return false;
+        }
+        return true;
+    }
+
+    public void ClearEmpty()
+    {
+        for (int i = 0; i < _members.Count;)
+        {
+            if (string.IsNullOrEmpty(_members[i].Type))
+                _members.RemoveAt(i);   // remove empty
+            else
+                ++i;
+        }
+    }
+
+    public void Append(IDeclear type)
+    {
+        _members.Add(type);
+    }
+
+    public bool Equals(ObjectDeclear other)
+    {
+        if (other == null)
+            return false;
+
+        if (_members.Count != other._members.Count)
+            return false;
+
+        for (int i = 0; i < _members.Count; ++i)
+        {
+            if (_members[i].Type != other._members[i].Type)
+                return false;
+            if (_members[i].Key != other._members[i].Key)
+                return false;
+        }
+        return true;
+    }
+
+    string GetDef()
+    {
+        if (_members.Count == 0)
+            return "null";
+        return string.Format("new {0}()", Type);
+    }
+}
+
+internal class ArrayDeclear : IDeclear
+{
+    string _key;
+    IDeclear _real = null;
+
+    public ArrayDeclear(string key) { _key = key; }
+    public string Type { get { return GetTypeName(); } }
+    public string Key { get { return _key; } }
+    public object Def { get { return GetDef(); } }
+    public DeclearType DeclearType { get { return DeclearType.Array; } }
+
+    public IDeclear Real { get { return _real; } }
+    public int MemberCount { get {return 1; }  }
+    public IDeclear[] Members { get { return new IDeclear[1] { _real }; } }
+
+    public void Append(IDeclear type)
+    {
+        if (_real == null)
+            _real = type;
+        else if (_real.Type != type.Type || _real.DeclearType != type.DeclearType)
+            throw new Exception(string.Format("array:{0} contain multy type:{1} deleartype:{2} elements", _key, type.Type, type.DeclearType));
+        else if (_real.DeclearType == DeclearType.Object)
+            Merge(_real as ObjectDeclear, type as ObjectDeclear);
+    }
+
+    void Merge(ObjectDeclear dst, ObjectDeclear src)
+    {
+        Dictionary<string, IDeclear> dstMembers = new Dictionary<string, IDeclear>();
+        Dictionary<string, IDeclear> srcMembers = new Dictionary<string, IDeclear>();
+
+        foreach (var mem in dst.Members)
+            dstMembers.Add(mem.Key, mem);
+        foreach (var mem in src.Members)
+            srcMembers.Add(mem.Key, mem);
+
+        // recursion merge object type
+        foreach (var pair in dstMembers)
+        {
+            if (pair.Value.DeclearType == DeclearType.Object && srcMembers.ContainsKey(pair.Key))
+                Merge(pair.Value as ObjectDeclear, srcMembers[pair.Key] as ObjectDeclear);
+        }
+
+        // add new types
+        foreach (var pair in srcMembers)
+        {
+            if (!dstMembers.ContainsKey(pair.Key))
+                dst.Append(pair.Value);
+        }
+    }
+
+    Action<IDeclear> FnGetMembers(Dictionary<string, IDeclear> dic)
+    {
+        return delegate (IDeclear type) { dic.Add(type.Key, type); };
+    }
+
+    string GetTypeName()
+    {
+        if (_real == null || string.IsNullOrEmpty(_real.Type))
+            return string.Empty;
+
+        if (_real.DeclearType != DeclearType.Array)
+            return _real.Type + "[]";
+
+        string name = ((ArrayDeclear)_real).GetTypeName();
+        if (string.IsNullOrEmpty(name))
+            return string.Empty;
+
+        return name + "[]";
+    }
+
+    string GetDef()
+    {
+        return "null";
+    }
+}
+
+internal class GeneralDeclear : IDeclear
+{
+    string _type;
+    string _key;
+    object _def;
+    public GeneralDeclear(string type, string key, object def)
+    {
+        _type = type;
+        _key = key;
+        _def = def;
+    }
+
+    public string Type { get { return _type; } }
+    public string Key { get { return _key; } }
+    public object Def { get { return _def; } }
+    public DeclearType DeclearType { get { return DeclearType.Basic; } }
+    public int MemberCount { get { return 0; } }
+    public IDeclear[] Members { get { return new IDeclear[0]; } }
+}
+
+internal class DeclearWalker : IJsonWalker
+{
+    Stack<IDeclear> _stack = new Stack<IDeclear>();
+    List<ObjectDeclear> _basicObjs = new List<ObjectDeclear>();
+    List<ObjectDeclear> _ComplexObjs = new List<ObjectDeclear>();
+    ObjectDeclear _dumy = new ObjectDeclear("");
+    ObjectDeclear _root = null;
+    string _key = "";
+
+    public ObjectDeclear Root { get { return _root; } }
+
+    public ObjectDeclear[] Report()
+    {
+        List<ObjectDeclear> namedTypes = new List<ObjectDeclear>();
+        if (_root == null)
+            return namedTypes.ToArray();
+
+        _basicObjs.Sort(delegate (ObjectDeclear l, ObjectDeclear r) { return l.MemberCount.CompareTo(r.MemberCount); });
+        _ComplexObjs.Sort(delegate (ObjectDeclear l, ObjectDeclear r) { return l.MemberCount.CompareTo(r.MemberCount); });
+
+        for (int i = 0; i < _basicObjs.Count; ++i)
+        {
+            ObjectDeclear obj = _basicObjs[i];
+            if (!string.IsNullOrEmpty(obj.Type))
+                continue;
+
+            obj.Type = string.Format("Type_{0}", namedTypes.Count + 1);
+            namedTypes.Add(obj);
+            for (int j = i + 1; j < _basicObjs.Count; ++j)
+            {
+                ObjectDeclear temp = _basicObjs[j];
+                if (obj.Equals(temp))
+                    temp.Type = obj.Type;
+            }
+        }
+
+        for (int i = 0; i < _ComplexObjs.Count; ++i)
+        {
+            ObjectDeclear obj = _ComplexObjs[i];
+            if (!string.IsNullOrEmpty(obj.Type))
+                continue;
+
+            obj.Type = string.Format("Type_{0}", namedTypes.Count + 1);
+            namedTypes.Add(obj);
+            for (int j = i + 1; j < _ComplexObjs.Count; ++j)
+            {
+                ObjectDeclear temp = _ComplexObjs[j];
+                if (obj.Equals(temp))
+                    temp.Type = obj.Type;
+            }
+        }
+
+        for (int i = 0; i < _ComplexObjs.Count; ++i)
+        {
+            _ComplexObjs[i].ClearEmpty();
+        }
+
+        namedTypes.Remove(_root);
+        return namedTypes.ToArray();
+    }
+
+    void Append(IDeclear type)
+    {
+        IDeclear top = _stack.Peek();
+        if (top.DeclearType == DeclearType.Object)
+            ((ObjectDeclear)top).Append(type);
+        else if (top.DeclearType == DeclearType.Array)
+            ((ArrayDeclear)top).Append(type);
+    }
+
+    public void OnObjectBegin()
+    {
+        ObjectDeclear type = new ObjectDeclear(_key);
+        _stack.Push(type);
+        if (_root == null)
+            _root = type;
+    }
+
+    public void OnObjectEnd()
+    {
+        ObjectDeclear type = _stack.Pop() as ObjectDeclear;
+        if (_stack.Count > 0)
+            Append(type);
+
+        if (type.IsBasic())
+            _basicObjs.Add(type);
+        else
+            _ComplexObjs.Add(type);
+    }
+
+    public void OnArrayBegin()
+    {
+        _stack.Push(new ArrayDeclear(_key));
+    }
+
+    public void OnArrayEnd()
+    {
+        ArrayDeclear type = _stack.Pop() as ArrayDeclear;
+        Append(type);
+
+        if (type.Real == null)
+        {
+            type.Append(_dumy);
+
+            if (!_basicObjs.Contains(_dumy))
+                _basicObjs.Add(_dumy);
+        }
+    }
+
+    public void OnIndex(int index) { }
+
+    public void OnKey(string key)
+    {
+        _key = key;
+    }
+
+    public void OnValue(float value)
+    {
+        Append(new GeneralDeclear("float", _key, "0.0f"));
+    }
+
+    public void OnValue(string value)
+    {
+        Append(new GeneralDeclear("string", _key, "string.Empty"));
+    }
+
+    public void OnValue(long value)
+    {
+        Append(new GeneralDeclear("long", _key, "0"));
+    }
+
+    public void OnValue(int value)
+    {
+        Append(new GeneralDeclear("int", _key, "0"));
+    }
+
+    public void OnValue(bool value)
+    {
+        Append(new GeneralDeclear("bool", _key, "false"));
+    }
+}
+
+public class JsonReport
+{
+    public IDeclear Root;
+    public IDeclear[] Manuals;
+}
+
+public class ExportJson
+{
+    const string FileName = "const.json";
+
+    public static JsonReport ExportJsonFile()
+    {
+        string jsonText = File.ReadAllText(FileName, Encoding.UTF8);
+        if (string.IsNullOrEmpty(jsonText))
+        {
+            Console.WriteLine("can not load json text from file:{0}", FileName);
+            return null;
+        }
+
+        JsonData data = JsonMapper.ToObject(jsonText);
+        if (data == null)
+        {
+            Console.WriteLine("parse json data failed from file:{0}", FileName);
+            return null;
+        }
+
+        DeclearWalker walker = new DeclearWalker();
+        Traverse("", data, walker);
+
+        JsonReport def = new JsonReport()
+        {
+            Root = walker.Root,
+            Manuals = walker.Report()
+        };
+        return def;
+    }
+
+    static void Traverse(string name, JsonData jd, IJsonWalker walker)
+    {
+        if (jd == null)
+            return;
+
+        walker.OnKey(name);
+
+        if (jd.IsObject)
+        {
+            walker.OnObjectBegin();
+            foreach (string key in jd.Keys)
+                Traverse(key, jd[key], walker);
+            walker.OnObjectEnd();
+        }
+        else if (jd.IsArray)
+        {
+            walker.OnArrayBegin();
+            for (int i = 0; i < jd.Count; ++i)
+            {
+                walker.OnIndex(i);
+                Traverse(name, jd[i], walker);
+            }
+            walker.OnArrayEnd();
+        }
+        else if (jd.IsBoolean)
+        {
+            walker.OnValue((bool)jd);
+        }
+        else if (jd.IsInt)
+        {
+            walker.OnValue((int)jd);
+        }
+        else if (jd.IsLong)
+        {
+            walker.OnValue((long)jd);
+        }
+        else if (jd.IsDouble)
+        {
+            walker.OnValue((float)(double)jd);
+        }
+        else if (jd.IsString)
+        {
+            walker.OnValue((string)jd);
+        }
+    }
+}
