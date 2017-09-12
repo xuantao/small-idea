18f3cbd7f35ea264977c59493f6d5c478101a6e1
xuantao <xuantao1@kingsoft.com>
2017-09-12 20:33:12 +0800

    setup project struct

---
+++ gcf/maker/utility/src/StringPool.cpp
@@ -0,0 +1,38 @@
+﻿#include "StringPool.h"
+#include <string>
+#include <set>
+
+CFG_NAMESPACE_BEGIN
+
+namespace utility
+{
+    namespace detail
+    {
+        class StringPoolImpl
+        {
+        public:
+            std::set<std::string> _strs;
+        };
+    }
+
+    StringPool::StringPool()
+    {
+        _pool = new detail::StringPoolImpl();
+    }
+
+    StringPool::~StringPool()
+    {
+        delete _pool;
+    }
+
+    const char* StringPool::Record(const char* str)
+    {
+        if (str == nullptr)
+            return nullptr;
+
+        auto pir = _pool->_strs.insert(std::string(str));
+        return pir.first->c_str();
+    }
+}
+
+CFG_NAMESPACE_END
