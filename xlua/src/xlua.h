08c53d2640579a2fbee479be2eb6e30962d3806d
xuantao.zh@gmail.com <xuantao.zh@gmail.com>
2019-03-05 13:42:26 +0800

    add xlua project

---
+++ xlua/src/xlua.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <lua.hpp>
+
+#define XLUA_NAMESPACE_BEGIN    namespace xlua {
+#define XLUA_NAMESPACE_END      } // namespace xlua
+
+XLUA_NAMESPACE_BEGIN
+
+
+
+XLUA_NAMESPACE_END
